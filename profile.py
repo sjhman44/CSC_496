77ec7a768e07ec3fec744cdee6912b509465274f
sjhman44 <sjhman44@users.noreply.github.com>
2019-12-03 12:52:06 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -2,32 +2,32 @@ import geni.portal as portal
 import geni.rspec.pg as pg
 import geni.rspec.igext as IG
 
-# Create a portal context.
 pc = portal.Context()
-
-# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
-
 tourDescription = \
 """
-This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
-At the moment, we start with a single node running MPI.
+This profile provides the template for a Hortonworks cluster with one 
+namenode and three datanode.
 """
-
-# Setup the Tour info with the above description and instructions.  
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-node = request.XenVM("compute-node")
-node.cores = 4
-node.ram = 4096
-    
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routable_control_ip = "true"
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
-node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
-# Print the RSpec to the enclosing page.
+for i in range(4):
+  if i == 0:
+    node = request.XenVM("namenode")
+  else:
+    node = request.XenVM("datanode-" + str(i))
+  node.cores = 4
+  node.ram = 4096
+  node.routable_control_ip = "true"  
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+
 pc.printRequestRSpec(request)
